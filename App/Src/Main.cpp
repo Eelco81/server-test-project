fb4b043550e8df4cef06665b46a046049fd561da
unknown <ENEDERKO@NLNVPC255.nl.moog.com>
2016-06-02 21:33:08 +0200

    previously added wrong main

--- App/Src/Main.cpp
+++ App/Src/Main.cpp
@@ -1,9 +1,24 @@
 
-#include "gtest/gtest.h"
+#include "Network.h"
+#include "Log.h"
 
-int main (int argc, char **argv) {
+#include "TcpServer.h"
+#include "SupportThread.h"
 
-	::testing::InitGoogleTest (&argc, argv);
+int main (int* argc, char** argv) {
+    
+    Core::Log::Instance ().Initialize (Core::Log::kTrace);
+    Core::Network::Initialize ();
 
-	return RUN_ALL_TESTS ();
+    App::SupportThread supportThread;
+    supportThread.Spawn ();
+
+    Api::Server server ("127.0.0.1", "1234");
+    server.Start ();
+
+    supportThread.Join ();
+    
+    Core::Network::Done ();
+
+    return 0;
 }
