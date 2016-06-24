ebb96ca9f592fa068faf166cda1b84a5ef5ec02f
enederko <enederkoorn@moog.com>
2016-06-24 13:28:15 +0200

    cleaned up makefiles

---
+++ Tools/build.mk
@@ -0,0 +1,58 @@
+
+CC              = g++
+AR              = ar
+CFLAGS          = -c -Wall -std=gnu++14
+
+PRODSOURCES     = $(filter-out %MockMain.cpp,$(filter-out %Tester.cpp,$(SOURCES)))
+PRODOBJECTS     = $(PRODSOURCES:.cpp=.o)
+
+GMOCKDIR        = ./../../../External/gmock/googlemock
+GMOCKSOURCES    = $(GMOCKDIR)/src/gmock-all.cc $(GMOCKDIR)/gtest/src/gtest-all.cc 
+GMOCKOBJECTS    = $(GMOCKSOURCES:.cc=.o)
+GMOCKINCLUDES   = -I$(GMOCKDIR) -I$(GMOCKDIR)/include -I$(GMOCKDIR)/gtest -I$(GMOCKDIR)/gtest/include 
+TESTOBJECTS     = $(SOURCES:.cpp=.o)
+TESTTARGET      = GoogleTest
+
+INCLUDES+=\
+    -I$(SOURCEDIR) \
+    $(GMOCKINCLUDES) \
+    $(patsubst %,-I$(LIBDIR)/%/Src,$(DEPENDENCIES)) 
+
+LIBPATHS=\
+    $(patsubst %,-L$(LIBDIR)/%/Make,$(DEPENDENCIES))
+
+LIBRARIES=\
+    $(patsubst %,-l%,$(shell echo $(DEPENDENCIES) | tr A-Z a-z))
+   
+# --- RULES ---
+
+$(DEPENDENCIES):
+	make all -C $(LIBDIR)/$@/Make
+
+$(LIBTARGET): $(DEPENDENCIES) $(PRODOBJECTS)  
+	$(AR) $(ARFLAGS) $@ $(PRODOBJECTS) 
+
+$(TARGET): $(DEPENDENCIES) $(PRODOBJECTS)  
+	$(CC) -o $@ $(APPOBJECTS) $(LIBPATHS) $(LIBRARIES) $(LINKS)
+    
+$(TESTTARGET): $(DEPENDENCIES) $(TESTOBJECTS) $(GMOCKOBJECTS) 
+	$(CC) -o $@ $(TESTOBJECTS) $(GMOCKOBJECTS) $(LIBPATHS) $(LIBRARIES) $(LINKS)
+    
+.cpp.o:
+	$(CC) $(CFLAGS) $(INCLUDES) $< -o $@
+
+.cc.o:
+	$(CC) $(CFLAGS) $(INCLUDES) $< -o $@
+
+# --- TARGETS ---
+
+all: $(LIBTARGET) $(TARGET)
+
+clean:
+	for lib in $(DEPENDENCIES) ; do \
+		make clean -C $(LIBDIR)/$$lib/Make ; \
+	done
+	rm -rf $(TESTOBJECTS) $(GMOCKOBJECTS) $(LIBTARGET) $(TARGET) $(TESTTARGET)
+    
+test: $(TESTTARGET)
+	./$(TESTTARGET)
