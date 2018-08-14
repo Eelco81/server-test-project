4cf13d7a23c31280f385e2e520c6cacb40ee5dbe
Eelco81 <eelco.nederkoorn@gmail.com>
2018-08-14 21:30:12 +0100

    Updated test specs

--- App/Test/features/connection.feature
+++ App/Test/features/connection.feature
@@ -3,8 +3,15 @@ Feature: Basic routing
 
   Scenario: Calling non existing endpoints
     When sending a GET request on /i/do/not/exist
-     Then the received response has code 404
-      And the received response contains a header with key 'Host'
-      And the received response contains a header with key 'Last-Modified'
-      And the received response contains header 'User-Agent: HttpServer/0.0.1'
-      And the received response contains header 'Content-Length: 0'
+    Then the received response has code 404
+     And the received response contains a header with key 'Host'
+     And the received response contains a header with key 'Last-Modified'
+     And the received response contains header 'User-Agent: HttpServer/0.0.1'
+     And the received response contains header 'Content-Length: 0'
+     And the received response contains all standard headers
+
+  Scenario: Calling the echo endpoint
+    When sending a PUT request on /system/echo
+    Then the received response has code 404
+     And the received response contains all standard headers
+
--- App/Test/steps/connection_steps.py
+++ App/Test/steps/connection_steps.py
@@ -3,9 +3,14 @@ import requests
 
 from behave import given, when, then, step
 
-@when('sending a GET request on {route}')
-def step_impl(context, route):
-    context.response = requests.get(context.url + route)
+@when('sending a {method} request on {route}')
+def step_impl(context, method, route):
+    if method == "GET" :
+        context.response = requests.get(context.url + route)
+    elif method == "PUT":
+        context.response = requests.put(context.url + route, data = "")
+    else :
+        assert false
 
 @then('the received response has code {code:d}')
 def step_impl(context, code):
@@ -18,3 +23,11 @@ def step_impl(context, key):
 @then('the received response contains header \'{key}: {value}\'')
 def step_impl(context, key, value):
     assert context.response.headers.get(key) == value
+    
+@then('the received response contains all standard headers')
+def step_impl(context):
+    assert context.response.headers.get('Last-Modified') != None
+    assert context.response.headers.get('User-Agent') == 'HttpServer/0.0.1'
+    print (context.response.headers.get('Host'))
+    assert context.response.headers.get('Host') == context.ip + ":" + context.port
+    
