2c5c417c12c3c666687f9d645603c3159ce1a1d1
Ëelco81 <enederkoorn@hotmail.com>
2019-02-01 23:14:14 +0000

    refactored main script

--- Web/src/script.js
+++ Web/src/script.js
@@ -1,65 +1,11 @@
 
 'use strict';
 
-import Vue from 'vue';
-import VueRouter from 'vue-router';
-
 import 'bootstrap';
-import $ from 'jquery';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import './css/style.css'
 
-import routes from './views/routes.js'; 
-import HeaderBar from './components/header-bar.js';
-import SideBar from './components/side-bar.js'; 
-import ChartArea from './components/chart-area.js';
-import PortTable from './components/port-table.js';
-import SimControl from './components/sim-control.js';
-import SimChart from './components/sim-chart.js';
-import SimXyChart from './components/sim-xy-chart.js';
+import App from './vue-app.js'
 
-import SimService from './models/sim-service.js'
-
-$(function() {
-    
-    Vue.use(VueRouter);
-    const router = new VueRouter({ routes: routes });
-    router.replace("sim-view");
-    
-    const simService = new SimService();
-    
-    new Vue({
-        el: '#vue-app',
-        router: router,
-        provide: function () {
-            return {
-                simService: function() {
-                    return simService;
-                }
-            }
-        },
-        components: {
-            'header-bar': HeaderBar,
-            'side-bar': SideBar,
-            'chart-area': ChartArea,
-            'port-table': PortTable,
-            'sim-control': SimControl,
-            'sim-chart': SimChart,
-            'sim-xy-chart': SimXyChart
-        },
-        template:`
-            <div>
-                <header-bar></header-bar>
-                <div class="container-fluid">
-                    <div class="row">
-                        <side-bar></side-bar>
-                        <router-view></router-view>
-                    </div>
-                </div>
-            </div>
-        `
-    });
-    
-});
+$(function() { new App('#vue-app'); });
---
+++ Web/src/vue-app.js
@@ -0,0 +1,57 @@
+
+'use strict';
+
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+import routes from './views/routes.js'; 
+import HeaderBar from './components/header-bar.js';
+import SideBar from './components/side-bar.js'; 
+import ChartArea from './components/chart-area.js';
+import PortTable from './components/port-table.js';
+import SimControl from './components/sim-control.js';
+import SimChart from './components/sim-chart.js';
+import SimXyChart from './components/sim-xy-chart.js';
+
+import SimService from './models/sim-service.js'
+
+Vue.use(VueRouter);
+
+export default function (div) {
+    
+    const router = new VueRouter({ routes: routes, base: "/home/" });
+    router.replace("sim-view");
+    
+    const simService = new SimService();
+    
+    return new Vue({
+        router: router,
+        provide: function () {
+            return {
+                simService: function() {
+                    return simService;
+                }
+            }
+        },
+        components: {
+            'header-bar': HeaderBar,
+            'side-bar': SideBar,
+            'chart-area': ChartArea,
+            'port-table': PortTable,
+            'sim-control': SimControl,
+            'sim-chart': SimChart,
+            'sim-xy-chart': SimXyChart
+        },
+        template:`
+            <div>
+                <header-bar></header-bar>
+                <div class="container-fluid">
+                    <div class="row">
+                        <side-bar></side-bar>
+                        <router-view></router-view>
+                    </div>
+                </div>
+            </div>
+        `
+    }).$mount(div);
+}
