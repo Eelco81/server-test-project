a74794e4c1417d5239a07d58b583754548d1f163
Ëelco81 <enederkoorn@hotmail.com>
2019-02-04 22:03:06 +0000

    Added source maps

--- Web/webpack.conf.js
+++ Web/webpack.conf.js
@@ -6,6 +6,8 @@ module.exports = {
     
     entry: './src/script.js',
     
+    devtool: "source-map",
+    
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, "dist")
