2fe2c41ec131b95509b9dd5e7ccbc33316ec8cb5
Ëelco81 <enederkoorn@hotmail.com>
2019-02-11 21:46:23 +0000

    removed temporary file from repository

--- Test/tmp.py
+++
@@ -1,8 +0,0 @@
-
-from selenium import webdriver
-import chromedriver_binary  # Adds chromedriver binary to path
-
-driver = webdriver.Chrome()
-driver.get("http://www.python.org")
-assert "Python" in driver.title
-driver.close()
