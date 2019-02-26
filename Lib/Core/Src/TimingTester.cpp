6239b1b349377c1b8251059232ebc9041365ca6a
Ëelco81 <enederkoorn@hotmail.com>
2019-02-26 22:32:43 +0000

    Expanded timing unit tests

--- Lib/Core/Src/TimingTester.cpp
+++ Lib/Core/Src/TimingTester.cpp
@@ -2,17 +2,18 @@
 #include "gtest/gtest.h"
 #include "Timing.h"
 
+class TimingTester : public ::testing::TestWithParam<uint64_t> {};
 
-TEST (Timing, Sleep) {
+INSTANTIATE_TEST_CASE_P (TimingTester, TimingTester,
+    ::testing::Values ( 1000u, 2000u, 3000u, 4000u, 5000u, 
+                        10000u, 20000u, 50000u, 100000u, 1000000u )
+);
 
-    
-    const uint64_t kSleepTime (1000000u);
-    
+TEST_P (TimingTester, Sleep) {
+
     const auto t1 (OS::Timing::Now ());
-    
-    OS::Timing::Sleep (kSleepTime);
-    
+    OS::Timing::Sleep (GetParam ());
     const auto t2 (OS::Timing::Now ());
-    
-    ASSERT_NEAR (t2 - t1, kSleepTime, 1000u);
+    ASSERT_GT (t2, t1);
+    ASSERT_NEAR (t2 - t1, GetParam(), 1500u);
 }
